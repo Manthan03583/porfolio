36363865307863de0aae2a8988ae19d06896d83a
manthan <manthanmoharana03583@gmail.com>
2025-06-25 22:36:25 +0530

    updated resume

--- components/Header.jsx
+++ components/Header.jsx
@@ -50,7 +50,7 @@ const Header = () => {
                 initial={{y: 30, opacity: 0}}
                 whileInView={{y: 0, opacity: 1}}
                 transition={{duration: 0.6, delay:1.2}} 
-                href="https://drive.google.com/file/d/1PKeJBRmLotRvyNA6jIZ7pdtF2VkUjj44/view?usp=sharing" download 
+                href="https://drive.google.com/file/d/1YYcj_k35TlszBqbwpk9nTh1hhSouQfrA/view?usp=sharing" download 
                 className='px-10 py-3 border rounded-full border-gray-500 flex 
                 items-center gap-2 bg-white dark:text-black'>
                     my resume <Image src={assets.download_icon} alt='' className='w-4'/> 
